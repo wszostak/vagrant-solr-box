3dd24b79155cd2e62f50b0efa2280fda32fa05ea
Wojciech Szóstak <wojciech.szostak@allegro.pl>
2014-09-16 09:23:06 +0200

    Upgraded to solr 4.10.0

--- manifests/default.pp
+++ manifests/default.pp
@@ -47,7 +47,7 @@ class must-have {
   }
 
   exec { "download_solr":
-    command => "curl -L http://apache.komsys.org/lucene/solr/4.7.0/solr-4.7.0.tgz | tar zx --directory=/vagrant/solr --strip-components 1",
+    command => "curl -L ftp://ftp.piotrkosoft.net/pub/mirrors/ftp.apache.org/lucene/solr/4.10.0/solr-4.10.0.tgz | tar zx --directory=/vagrant/solr --strip-components 1",
     cwd => "/vagrant",
     user => "vagrant",
     path => "/usr/bin/:/bin/",
